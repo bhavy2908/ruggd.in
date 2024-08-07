614ff3d831ed8a39299bedf3c5b2aa3f29213ab6
Bhavy <bhavy2908@gmail.com>
2024-08-07 15:24:01 +0530

    Update Carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,148 +1,98 @@
-'use client'
-import React, { useState } from "react";
-import "../assets/Carousel.css"
-
-interface CarouselItem {
-  imageUrl: string;
-  link: string;
-  index: number;
-}
+"use client";
+import React, { useEffect } from "react";
+import "../assets/Carousel.css";
 
 const CarouselComponent: React.FC = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const carouselItems: CarouselItem[] = [
+  const carouselItems = [
     {
-      imageUrl:
-        "https://5.imimg.com/data5/SELLER/Default/2023/5/305639688/VA/II/TR/68642660/versace-luxury-rug-500x500.jpg",
-      link: "",
-      index: 1,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://media.karousell.com/media/photos/products/2021/5/20/formula_1_wool_rug_thick_pile_1621476763_723a3767_progressive.jpg",
-      link: "",
-      index: 2,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://5.imimg.com/data5/SELLER/Default/2023/9/344842391/OW/AY/XG/87155868/rfgtht-500x500.jpg",
-      link: "",
-      index: 3,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://letsrug.in/cdn/shop/files/014E4F37-FB2C-4C1E-A005-A2FBE026D6EF.png?v=1695576857&width=416",
-      link: "",
-      index: 4,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1AkAcOzKFrnPOrcIqj9AlGgzmogVMN1OhA&s",
-      link: "",
-      index: 5,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://i.etsystatic.com/33539386/r/il/a127ec/4123396508/il_fullxfull.4123396508_i0jr.jpg",
-      link: "",
-      index: 6,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGttiFqHhETyzBwECFStbCL1GsVCLWI3PmNg&s",
-      link: "",
-      index: 7,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://m.media-amazon.com/images/I/71qKIhTTRJS._AC_UF894,1000_QL80_.jpg",
-      link: "",
-      index: 8,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
     {
-      imageUrl:
-        "https://www.travelsintranslation.com/wp-content/uploads/2021/07/Punk-Rock-Stars-Social-Distortion-Band-Area-Rug-Music-CV60024-Rug-Carpet.jpg",
-      link: "",
-      index: 9,
-    },
-    {
-      imageUrl:
-        "https://i.etsystatic.com/36437972/r/il/1554f8/4100559936/il_fullxfull.4100559936_sju7.jpg",
-      link: "",
-      index: 10,
-    },
-    {
-      imageUrl:
-        "https://preview.redd.it/i-made-a-gta-san-andreas-rug-v0-88y634hihb091.jpg?width=640&crop=smart&auto=webp&s=996696297d9500c506850251d4512a3b8ed95dee",
-      link: "",
-      index: 11,
+      src: "http://static.superiorwallpapers.com/images/lthumbs/2016-03/11645_Elephant-runing-in-the-jungle-HD-wild-animal-wallpaper.jpg",
+      alt: "Elephant running in the jungle",
     },
   ];
 
-  const handleControlClick = (direction: "left" | "right") => {
-    setActiveIndex((prevIndex) => {
-      if (direction === "left") {
-        return prevIndex === 0 ? carouselItems.length - 1 : prevIndex - 1;
-      } else {
-        return prevIndex === carouselItems.length - 1 ? 0 : prevIndex + 1;
-      }
+  useEffect(() => {
+    const swiftUpElements = document.querySelectorAll(".swift-up-text");
+    swiftUpElements.forEach((elem) => {
+      const words = elem.textContent!.split(" ");
+      elem.innerHTML = "";
+      words.forEach((el, index) => {
+        words[index] = `<span><i>${words[index]}</i></span>`;
+      });
+      elem.innerHTML = words.join(" ");
+      const children = document.querySelectorAll("span > i");
+      children.forEach((node, index) => {
+        (node as HTMLElement).style.animationDelay = `${index * 0.2}s`;
+      });
     });
-  };
+  }, []);
 
   return (
-    <div className="carousel">
-      <div className="lamp-img">
-        <img
-          src="https://i.pinimg.com/474x/24/ed/e0/24ede0000cc3e1b22582fa5f36cfb6f2.jpg"
-          alt="Lamp"
-        />
-      </div>
-
-      <div className="carousel-control-button left">
-        <input
-          type="radio"
-          name="carousel-control-input"
-          onChange={() => handleControlClick("left")}
-        />
-      </div>
-      <div className="carousel-control-button right">
-        <input
-          type="radio"
-          name="carousel-control-input"
-          onChange={() => handleControlClick("right")}
-          checked={true}
-        />
-      </div>
-
-      <div className="carousel-rotation-direction">
-        <ul
-          className="carousel-item-wrapper"
-          style={
-            { "--_num-elements": carouselItems.length } as React.CSSProperties
-          }
-        >
+    <div className="container-box">
+      <div className="container2">
+        <div className="lamp-img">
+          <img
+            src="https://i.pinimg.com/474x/24/ed/e0/24ede0000cc3e1b22582fa5f36cfb6f2.jpg"
+            alt="Lamp"
+          />
+        </div>
+        <div id="carousel">
           {carouselItems.map((item, index) => (
-            <li
-              key={item.index}
-              className={`carousel-item ${
-                index === activeIndex ? "active" : ""
-              }`}
+            <div
+              key={index}
               style={
                 {
-                  "--_index": item.index,
-                  "--_image-url": `url('${item.imageUrl}')`,
+                  "--_image-url": `url(${item.src})`,
                 } as React.CSSProperties
               }
             >
-              <a href={item.link} target="_blank" rel="noopener noreferrer">
-                {item.index}
-              </a>
-            </li>
+              <img src={item.src} alt={item.alt} loading="lazy" />
+            </div>
           ))}
-          <li className="carousel-ground"></li>
-        </ul>
+          <div className="carousel-ground"></div>
+        </div>
       </div>
+      <section className="container3">
+        <h1 className="swift-up-text">unleash the</h1>
+        <h1 className="swift-up-text" style={{ color: "#186F65" }}>
+          elegance
+        </h1>
+        <h1 className="swift-up-text">
+          underfoot.
+        </h1>
+        <button className="swift-up-text shop-now-btn">Discover Now</button>
+      </section>
     </div>
   );
 };
